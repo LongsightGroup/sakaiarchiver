cb44d0734cd9e10914f70e2ded2c5e914553c927
Greg Monroe <monroe@longsight.com>
2013-11-14 16:50:14 -0500

    Support for syllabus tool

--- src/main/java/org/sakaiproject/util/archiver/parsers/SyllabusParser.java
+++ src/main/java/org/sakaiproject/util/archiver/parsers/SyllabusParser.java
@@ -1,10 +1,20 @@
 package org.sakaiproject.util.archiver.parsers;
 
+import java.net.URI;
+
+import org.apache.commons.io.FilenameUtils;
+import org.sakaiproject.util.archiver.Archiver;
+import org.sakaiproject.util.archiver.ParsingUtils;
 import org.sakaiproject.util.archiver.ToolParser;
+
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 public class SyllabusParser extends ToolParser {
 
+    public static final int MAIN_PAGE = 1;
+
 	public static final String TOOL_NAME = "syllabus";
+
 	@Override
 	public void initialize() {
 		setSubdirectory(getToolName());
@@ -14,4 +24,35 @@ public class SyllabusParser extends ToolParser {
 	public String getToolName() {
 		return TOOL_NAME;
 	}
+
+    @Override
+    public void parse() throws Exception {
+        super.parse();
+        setToolPageName(FilenameUtils.getName(new URI(getToolURL()).getPath()));
+        HtmlPage mainPage = loadToolMainPage();
+
+        // Overwrite the default frame with the updated version.
+        String name = getSubdirectory() + getToolPageName();
+        msg("Updating main roster iframe: " + name + "(" +
+                mainPage.getTitleText()+")", Archiver.NORMAL);
+        savePage(MAIN_PAGE, mainPage, name);
+    }
+
+    @Override
+    public String addJavascript() throws Exception {
+        String js = "";
+        String script = "";
+        switch ( getPageSaveType() ) {
+            case MAIN_PAGE:
+                script = "\\$(\"UL.navIntraTool\").after(\""
+                    + "<div id='syllabusInfo'>Note: The links in the syllabus "
+                    + "bars below are all click-able (even though they are not in "
+                    + "red).  The edit options may open popups and appear to "
+                    + "work, but they will permanenty change things.  A page "
+                    + "reload will undo them.</div>\");";
+                js = ParsingUtils.addInlineJavaScript(script);
+                break;
+        }
+        return js;
+    }
 }
